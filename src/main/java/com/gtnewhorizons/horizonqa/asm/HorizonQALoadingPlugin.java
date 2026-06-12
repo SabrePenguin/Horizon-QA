3abec3c0d8c50ea02bf4793ed095de5c93d586ba
SabrePenguin <lknofczynski@gmail.com>
2026-06-12 13:50:13 -0600

    Fix template loading and server mixins

--- src/main/java/com/gtnewhorizons/horizonqa/asm/HorizonQALoadingPlugin.java
+++ src/main/java/com/gtnewhorizons/horizonqa/asm/HorizonQALoadingPlugin.java
@@ -1,13 +1,18 @@
 package com.gtnewhorizons.horizonqa.asm;
 
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import zone.rong.mixinbooter.IEarlyMixinLoader;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 
+@IFMLLoadingPlugin.Name("HorizonQA")
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 @IFMLLoadingPlugin.SortingIndex(11)
-public class HorizonQALoadingPlugin implements IFMLLoadingPlugin {
+@IFMLLoadingPlugin.TransformerExclusions("com.gtnewhorizon.horizonqa.asm")
+public class HorizonQALoadingPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
     @Override
     public String[] getASMTransformerClass() {
@@ -30,5 +35,10 @@ public class HorizonQALoadingPlugin implements IFMLLoadingPlugin {
     @Override
     public String getSetupClass() {
         return null;
+    }
+
+    @Override
+    public List<String> getMixinConfigs() {
+        return Collections.singletonList("mixins.horizonqa.json");
     }
 }
