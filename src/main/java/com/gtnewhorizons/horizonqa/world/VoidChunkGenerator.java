e057fe5a5f6abae744ab4514b5cba1f6ac7d1aee
SabrePenguin <lknofczynski@gmail.com>
2026-06-12 13:51:18 -0600

    Set to only plains biome

--- src/main/java/com/gtnewhorizons/horizonqa/world/VoidChunkGenerator.java
+++ src/main/java/com/gtnewhorizons/horizonqa/world/VoidChunkGenerator.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.init.Biomes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
@@ -19,21 +20,20 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @MethodsReturnNonnullByDefault
 public class VoidChunkGenerator implements IChunkGenerator {
 
-    private final World worldObj;
+    private final World world;
 
     public VoidChunkGenerator(World world) {
-        this.worldObj = world;
+        this.world = world;
     }
 
     @Override
-    public Chunk generateChunk(int chunkX, int chunkZ) {
-        ChunkPrimer primer = new ChunkPrimer();
-        Chunk chunk = new Chunk(worldObj, primer, chunkX, chunkZ);
+    public Chunk generateChunk(int x, int z) {
+        ChunkPrimer chunkprimer = new ChunkPrimer();
+        Chunk chunk = new Chunk(this.world, chunkprimer, x, z);
         chunk.generateSkylightMap();
         byte[] abyte = chunk.getBiomeArray();
-        Biome[] biomes = worldObj.getBiomeProvider().getBiomes(null, chunkX << 4, chunkZ << 4, 16, 16);
-        for (int i = 0; i < abyte.length; ++i) {
-            abyte[i] = (byte) Biome.getIdForBiome(biomes[i]);
+        for (int i1 = 0; i1 < abyte.length; ++i1) {
+            abyte[i1] = (byte) Biome.getIdForBiome(Biomes.PLAINS);
         }
         chunk.generateSkylightMap();
         return chunk;
